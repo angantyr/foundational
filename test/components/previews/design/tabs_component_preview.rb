49ee4ae1dfb77fbcf723f34f892cf87c7c1770e1
Mans Angantyr <mans@angantyr.com>
2025-11-28 14:13:35 -0500

    Add Design::Tabs component with horizontal and vertical variants
    Created a simple tabs component wrapping css-zero's tabs widget with
    support for both horizontal (default) and vertical layout variants.
    
    - Component uses css-zero's tabs_controller.js for tab switching
    - Horizontal variant: standard inline-flex tabs (css-zero default)
    - Vertical variant: flex-row container with column-stacked tabs
    - Lookbook previews for both variants

---
+++ test/components/previews/design/tabs_component_preview.rb
@@ -0,0 +1,11 @@
+class Design::TabsComponentPreview < Lookbook::Preview
+  # @label Default
+  def default
+    render_with_template
+  end
+
+  # @label Vertical
+  def vertical
+    render_with_template
+  end
+end
